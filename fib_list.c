9d9f6fb02a4710a25e35429fc48d99d88c265d07
chen-yehe <50775916+chen-yehe@users.noreply.github.com>
2019-05-19 12:07:36 +0300

    Update fib_list.c

--- fib_list.c
+++ fib_list.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 #include <malloc.h>
-#include "Node.h"
 
-double n;
+
 struct Node *head, *tail, *curr;
-
 void createFibList(int);
 void printFibList(struct Node *head);
+double n;
+
+
+/*Linked list node */
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+
+/*
+*This program recieves a positive integer number from the user, creates a circular
+*linked list that represents the fiboncci sequence of that number, and finally prints it.
+*/
 
 int main() {
     printf("\n*****WELCOME TO FIBONACCI PROGRAM!***** \n\n");
     printf("Please enter a POSITIVE INTEGER: \n");
     scanf("%le", &n);
 
-    if((ceil(n) == n) && n > 0){
+    if((ceil(n) == n) && n > 0){ /*The ceiling value of n equals to n, therefor, n is an integer.*/
         createFibList(n);
         printFibList(head);
 
     }
 
-    else{
+    else{ /*If the input is not integer or negtive, exit the program*/
         printf("\nERROR. Invalid input.\n");
     }
 
@@ -28,6 +40,10 @@ int main() {
 }
 
 
+/*
+*The function gets a number and creates a circular
+*linked list that represents the fiboncci sequence of that number.
+*/
 void createFibList(int n){
     struct Node *beforePrev, *prev;
     int i;
@@ -36,7 +52,7 @@ void createFibList(int n){
         struct Node *currNode = NULL;
         currNode = malloc(sizeof(struct Node));
 
-        if(i == 0 || i == 1){
+        if(i == 0 || i == 1){ /*two first nodes equal to 1*/
             currNode->data = 1;
             if(i == 0){
                 tail = currNode;
@@ -47,7 +63,7 @@ void createFibList(int n){
             }
         }
 
-        else{
+        else{ /*curr node data equals to beforePrev + prev. move beforePrev and prev to the next node.*/
             currNode->data = beforePrev->data + prev->data;
             currNode->next = prev;
             beforePrev = prev;
@@ -55,17 +71,22 @@ void createFibList(int n){
         }
     }
 
+    /*Connects head and tail*/
     head = prev;
     tail->next = head;
 }
 
+/*
+The function gets a parameter, which is a pointer to the head of a circular linked list and prints it.
+*/
 void printFibList(struct Node *head){
     int size = (int)n;
     printf("\nPrinting the fibonacci list for n = %d :\n\n", size);
     curr = head;
     do{
         printf("%d \n", curr->data);
+	free(curr);
         curr = curr->next;
 
-    }while(curr != head);
+    }while(curr != head); /*as long as curr pointer doesn't get back to head*/
 }
